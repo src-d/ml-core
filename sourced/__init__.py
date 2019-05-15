1165f0963bc47dd74f90c081ca7c4ed901a360bb
Guillem Duran <guillem@sourced.tech>
2019-05-15 16:18:04 +0200

    Fix package namespace
    Signed-off-by: Guillem Duran <guillem@sourced.tech>

--- sourced/__init__.py
+++ sourced/__init__.py
@@ -1,1 +1,3 @@
 """Common namespace for sourced tools."""
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
--- sourced/ml/__init__.py
+++ sourced/ml/__init__.py
@@ -1,1 +1,3 @@
 """MLonCode research playground."""
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
